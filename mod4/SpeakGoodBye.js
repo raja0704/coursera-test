a7fa442223a26286e4476463b4dcbb1cd95f0d0c
Raja Vardhan Reddy <56930999+raja0704@users.noreply.github.com>
2020-08-09 16:59:57 +0530

    Delete SpeakGoodBye.js

--- mod4/SpeakGoodBye.js
+++
@@ -1,7 +0,0 @@
-(function (window) {
-    var byeSpeaker = {};
-    byeSpeaker.speak = function (names) {
-        console.log("Goodbye "+ " " +names);
-    }
-    window.byeSpeaker = byeSpeaker;
-})(window);
